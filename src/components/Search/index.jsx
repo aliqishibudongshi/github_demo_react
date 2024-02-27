8ebcf6e52e5f46c11b1b49a2e547523afa4aab29
SPZ <450067501@qq.com>
2024-02-27 15:29:34 +0800

    second version - use pubsub substitute parenting-child communication

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,22 +1,26 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import PubSub from 'pubsub-js';
+import axios from 'axios';
+
 
 export default class Search extends Component {
-
 	search = ()=>{
 		//获取用户的输入(连续解构赋值+重命名)
 		const {keyWordElement:{value:keyWord}} = this
-		//发送请求前通知App更新状态
-		this.props.updateAppState({isFirst:false,isLoading:true})
+		//发送请求前通知List更新状态
+		// this.props.updateAppState({isFirst:false,isLoading:true});
+		PubSub.publish("updateState", {isFirst:false,isLoading:true});
 		//发送网络请求
 		axios.get(`https://api.github.com/search/users?q=${keyWord}`).then(
 			response => {
-				//请求成功后通知App更新状态
-				this.props.updateAppState({isLoading:false,users:response.data.items})
+				//请求成功后通知List更新状态
+				// this.props.updateAppState({isLoading:false,users:response.data.items})
+				PubSub.publish("updateState", {isLoading:false,users:response.data.items});
 			},
 			error => {
-				//请求失败后通知App更新状态
-				this.props.updateAppState({isLoading:false,err:error.message})
+				//请求失败后通知List更新状态
+				this.props.updateAppState({isLoading:false,err:error.message});
+				PubSub.publish("updateState", {isLoading:false,err:error.message});
 			}
 		)
 	}
